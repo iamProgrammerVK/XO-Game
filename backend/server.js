5ff548ff35da03dc20b299fb078ba195f84a82a1
VISHNUKUMAR A <114072441+iamProgrammerVK@users.noreply.github.com>
2025-11-12 19:01:08 +0530

    Update CORS settings and server port configuration

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,27 @@ import { Server } from "socket.io";
 import cors from "cors";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ 
+  origin: [
+    "http://localhost:3000",
+    "https://xo-game-vk.vercel.app",
+  ],
+})
+);
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+  cors: { origin: [
+    "http://localhost:3000",
+    "https://xo-game-vk.vercel.app",
+  ], 
+  methods: ["GET", "POST"] },
 });
+
+app.get("/", (req, res) => {
+  res.send("XO Multiplayer Backend is running 🎮");
+});
+
 
 const rooms = {};
 
@@ -52,5 +67,7 @@ io.on("connection", (socket) => {
     console.log("Disconnected:", socket.id);
   });
 });
-
-server.listen(5000, () => console.log("✅ Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () =>
+  console.log(`✅ Server running on port ${PORT} or Render`)
+);
